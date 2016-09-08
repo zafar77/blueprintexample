47e21710db850ab440ed4e5e069ebfa37218da5a
CloudYuga Technologies <neependra@cloudyuga.guru>
2016-09-08 16:15:30 +0530

    Update blueprintexample.py

--- blueprintexample.py
+++ blueprintexample.py
@@ -2,6 +2,7 @@ from flask import Flask
 from simple_page.simple_page import simple_page
 
 app = Flask(__name__)
+##
 app.register_blueprint(simple_page)
 # Blueprint can be registered many times
 app.register_blueprint(simple_page, url_prefix='/pages')
